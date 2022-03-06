dc84e53e9e04a65a7bdd00361e00ade6afc6971b
KimSeokWu <potato4967@hanmail.net>
2022-03-06 23:26:11 +0900

    add parser

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pandas as pd
 import torch
 from generator import *
@@ -6,11 +8,16 @@ from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
 import train
 import utils.datasets as datasets
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch', type=int, default=4, help='n of epochs for fine tuning')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size for datasets')
+parser.add_argument('--lr', type=float, default=3e-5, help='learning rate for fine tuning')
+parser.add_argument('--data_file_path', type=str, default='dataset/k_youtube_datasets.csv', help='path where load datasets')
+parser.add_argument('--save_path', type=str, default='model_file/final_model', help='path where save the tuned model file')
+parser.add_argument('--load_path', type=str, default='model_file/final_model/pytorch_model.bin', help='path where load the model file')
+args = parser.parse_args()
+
 MODEL_PATH = 'skt/kogpt2-base-v2'
-FILE_PATH = 'dataset/k_youtube_datasets.csv'
-EPOCH = 4
-BATCH_SIZE = 32
-lr = 0.00003
 
 model = GPT2LMHeadModel.from_pretrained(MODEL_PATH)
 tokenizer = PreTrainedTokenizerFast.from_pretrained(MODEL_PATH,
@@ -20,16 +27,16 @@ tokenizer = PreTrainedTokenizerFast.from_pretrained(MODEL_PATH,
                                                     pad_token='<pad>',
                                                     mask_token='<mask>')
 
-data = pd.read_csv(FILE_PATH)
+data = pd.read_csv(args.data_file_path)
 docs = data['vid_name']
 train_dataset = datasets.TitleDataset(docs, tokenizer)
 
 # train model
-model = train.train_model(model, train_dataset, batch_size=BATCH_SIZE, epochs=EPOCH, learning_rate=lr)
-model.save_pretrained('model_file/final_model')
+model = train.train_model(model, train_dataset, batch_size=args.batch_size, epochs=args.epoch, learning_rate=args.lr)
+model.save_pretrained(args.save_path)
 
 # load model
-checkpoint = torch.load('model_file/final_model/pytorch_model.bin', map_location=torch.device('cpu'))
+checkpoint = torch.load(args.load_path, map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint)
 
 if __name__ == "__main__":
